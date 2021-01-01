611bd307b42088380dbc5f4f8f32b1487d91fd83
cbayonao <drcamilob@live.com>
2020-12-31 21:31:23 -0500

    [Adding] config items

--- config/index.js
+++ config/index.js
@@ -3,6 +3,11 @@ require('dotenv').config();
 const config = {
     dev: process.env.NODE_ENV !== 'produccion',
     port: process.env.PORT || 3000,
+    CORS: process.env.CORS,
+    dbUser: process.env.DB_USER,
+    dbPasswd: process.env.DB_PASSWD,
+    dbHost: process.env.DB_HOST,
+    dbName: process.env.DB_NAME,
 };
 
 module.exports = { config };
